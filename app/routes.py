0f17bc39f0a84580cc48177ce422464ba1c9c810
JL6850 <jl6850@columbia.edu>
2024-11-21 17:48:41 -0500

    Update Reset password token methods.

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,9 @@ from urllib.parse import urlsplit
 import sqlalchemy as sa
 from app import app, db
 from app.errors import internal_error
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm
 from app.models import User, Post
+from app.email import send_password_reset_email
 from datetime import datetime, timezone
 
 @app.route('/', methods=['GET', 'POST'])
@@ -155,3 +156,19 @@ def explore():
 		if posts.has_prev else None
 	return render_template("index.html", title='Explore', posts=posts.items,
 						   next_url=next_url, prev_url=prev_url)
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = ResetPasswordRequestForm()
+	if form.validate_on_submit():
+		user = db.session.scalar(
+			sa.select(User).where(User.email == form.email.data))
+		if user:
+			send_password_reset_email(user)
+		flash('Check your email for the instructions to reset your password')
+		return redirect(url_for('login'))
+	return render_template('reset_password_request.html',
+						   title='Reset Password', form=form)
+
